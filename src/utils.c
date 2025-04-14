1e76444cae8b95b444ecb5a9932bdcd41fe26459
IgorPrajnaDev <igor.melo@academico.ufpb.br>
2025-04-14 14:42:18 -0300

    correção do código removendo atribuição de 0 a caracteres que nao são letras

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "utils.h"
 
 int* text_to_numbers(const char* text) {
     int i = 0;
@@ -14,14 +15,11 @@ int* text_to_numbers(const char* text) {
 
     // Converter cada letra
     for (int j = 0; j < i; j++) {
+        
         char c = toupper(text[j]); // garante que seja maiúscula
-
-        if (c >= 'A' && c <= 'Z') {
-            numbers[j] = c - 'A' + 1;
-        } else {
-            numbers[j] = 0; // caractere não-alfabético vira 0
-        }
+        numbers[j] = c - 'A' + 1;
+        
     }
-
     return numbers;
-    }
+    
+}
